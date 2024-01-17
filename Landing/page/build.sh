42d238242a663b3936b391a613839a0a99436de0
Damilaug <dammyoladipupo15@gmail.com>
2024-01-17 15:20:16 +0100

    Bin added to the build.sh file

--- Landing/page/build.sh
+++ Landing/page/build.sh
@@ -1,4 +1,14 @@
-set -o errexit
-pip install -r requirements.txt 
+#!/usr/bin/env bash
+
+set -o errexit  # exit on error
+
+pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+# set -o errexit
+# pip install -r requirements.txt 
+# python manage.py collectstatic --no-input
+# python manage.py migrate
